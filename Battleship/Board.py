abe1f8e85822feb659802700ad497ed11f8d9307
Programming <programming@US-Programming-SETUP-3.local>
2017-09-18 13:40:05 -0400

    v. 0.1

--- Battleship/Board.py
+++ Battleship/Board.py
@@ -5,3 +5,7 @@ class Board:
 	def __init__(self, sizeX, sizeY):
 		self.boardP1 = [[for y in range(sizeY)] for x in range(sizeX)]
 		self.boardP2 = [[for y in range(sizeY)] for x in range(sizeX)]
+
+	def reset(self):
+		self.boardP1[:][:] = None
+		self.boardP2[:][:] = None
--- Battleship/Ship.py
+++ Battleship/Ship.py
@@ -1,3 +1,10 @@
 #v. 0.1  d. 9/18/17
 
 class Ship:
+
+	def hit(index, isHit):
+		isHit.pop(index)
+		isHit.insert(index, False)
+		return isHit
+
+	
