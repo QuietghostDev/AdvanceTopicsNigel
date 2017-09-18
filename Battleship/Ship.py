3dc39812850b82ef523f940244f5050a6c664cce
Programming <programming@US-Programming-SETUP-3.local>
2017-09-18 14:05:58 -0400

    v 0.1

--- Battleship/Ship.py
+++ Battleship/Ship.py
@@ -1,10 +1,27 @@
-#v. 0.1  d. 9/18/17
+#v 0.1  d. 9/18/17
 
 class Ship:
-
 	def hit(index, isHit):
 		isHit.pop(index)
 		isHit.insert(index, False)
 		return isHit
 
-	
+class Battleship(Ship):
+	def __init__(self):
+		size = 4
+
+class Submarine(Ship):
+	def __init__(self):
+		size = 3
+
+class Carrier(Ship):
+	def __init__(self):
+		size = 5
+
+class Cruiser(Ship):
+	def __init__(self):
+		size = 3
+
+class Destoryer(Ship):
+	def __init__(self):
+		size = 2
