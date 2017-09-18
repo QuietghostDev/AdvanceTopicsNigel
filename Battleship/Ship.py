42c4854cba7d8f5758f85c7dbb5018e0415a3fbe
Nigel Hamilton <quietghost1642@gmail.com>
2017-09-18 18:49:56 -0400

    v 0.1

--- Battleship/Ship.py
+++ Battleship/Ship.py
@@ -1,27 +1,41 @@
 #v 0.1  d. 9/18/17
 
 class Ship:
+
+	def __init__(self, size):
+		self.size = size
+		self.isHit = [False for i in range(size)]
+	
 	def hit(index, isHit):
 		isHit.pop(index)
-		isHit.insert(index, False)
+		isHit.insert(index, True)
 		return isHit
 
+		
 class Battleship(Ship):
-	def __init__(self):
-		size = 4
 
+	def __init__(self):
+		Ship.__init__(4)
+		
+		
 class Submarine(Ship):
-	def __init__(self):
-		size = 3
 
+	def __init__(self):
+		Ship.__init__(3)
+		
 class Carrier(Ship):
-	def __init__(self):
-		size = 5
 
+	def __init__(self):
+		Ship.__init__(5)
+
+		
 class Cruiser(Ship):
-	def __init__(self):
-		size = 3
 
-class Destoryer(Ship):
 	def __init__(self):
-		size = 2
+		Ship.__init__(3)
+
+		
+class Destoryer(Ship):
+
+	def __init__(self):
+		Ship.__init__(2)
