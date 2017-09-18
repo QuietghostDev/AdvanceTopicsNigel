ea09a72183a77b410cd3162ff5fad9811915d987
Programming <programming@US-Programming-SETUP-3.local>
2017-09-18 12:53:21 -0400

    oop

---
+++ Battleship/Board.py
@@ -0,0 +1,7 @@
+#v. 0.1  d. 9/18/17
+
+class Board:
+
+	def __init__(self, sizeX, sizeY):
+		self.boardP1 = [[for y in range(sizeY)] for x in range(sizeX)]
+		self.boardP2 = [[for y in range(sizeY)] for x in range(sizeX)]
