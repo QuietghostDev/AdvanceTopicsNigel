944d110680ea84aa30c923e2795b357417e7adb3
Programming <programming@US-Programming-SETUP-3.local>
2017-09-19 09:16:02 -0400

    super?

--- Battleship/Ship.py
+++ Battleship/Ship.py
@@ -4,30 +4,31 @@ class Ship:
 	def __init__(self, size):
 		self.size = size
 		self.isHit = [False for i in range(size)]
-	
+
 	def hit(index, isHit):
 		isHit.pop(index)
 		isHit.insert(index, True)
 		return isHit
-		
+
 class Battleship(Ship):
 	def __init__(self):
+		#super(Battleship, self).__init__(4)
 		Ship.__init__(4)
-		
+
 class Submarine(Ship):
 	def __init__(self):
 		Ship.__init__(3)
-		
+
 class Carrier(Ship):
 	def __init__(self):
 		Ship.__init__(5)
 
-		
+
 class Cruiser(Ship):
 	def __init__(self):
 		Ship.__init__(3)
 
-		
+
 class Destoryer(Ship):
 	def __init__(self):
 		Ship.__init__(2)
