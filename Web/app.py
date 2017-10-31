166c1fd038617f3a9429f513fcb8d09ea6f94be5
Nigel Hamilton <nhamilton18@germantownfriends.org>
2017-10-31 11:06:28 -0400

    Model reworking

--- Web/app.py
+++ Web/app.py
@@ -1,22 +1,25 @@
 from flask import Flask
-from models import Base, User
+from models import Base, Aircraft
 from flask_sqlalchemy import SQLAlchemy
 
-app =  Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
 
 @app.before_first_request
 def setup():
     Base.metadata.drop_all(bind=db.engine)
     Base.metadata.create_all(bind=db.engine)
-    db.session.add(Aircraft('B738', 'Boeing 737-800', 'N990NN', 'American Airlines'))
+    db.session.add(Aircraft('B738', 'Boeing 737-800', 'N990NN', 'American Airlines', True))
     db.session.commit()
+
 
 @app.route('/')
 def root():
     users = db.session.query(User).all()
-    return u"<br>".join([u"{0}: {1}".format(user.name, user.email) for user in users])
+    return u"<br>".join([u"{0}: {1}, {2}".format(aircraft.reg, aircraft.iata, aircraft.icao) for aircraft in Aircraft])
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000)
--- Web/models.py
+++ Web/models.py
@@ -1,23 +1,26 @@
-from sqlalchemy import Column, Interger, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Aircraft(Base):
-    __tablename__ = "aircraft"
+    __tablename__ = "Aircraft"
 
     icao = Column(String(4))
     iata = Column(String(20))
-    reg = Column(String(7), unique=True)
-    age = Column(Interger)
+    reg = Column(String(7), unique=True, primary_key=True)
+    age = Column(Integer)
     airline = Column(String(30))
+    status = Column(Boolean)
 
-    def __init__(self, icao, iata, reg, age, airline):
+    def __init__(self, icao, iata, reg, age, airline, status):
         self.icao = icao
         self.iata = iata
         self.reg = reg
         self.age = age
         self.airline = airline
+        self.status = status
 
     def __repr__(self):
         return '<Aircraft %r>' % (self.reg)
